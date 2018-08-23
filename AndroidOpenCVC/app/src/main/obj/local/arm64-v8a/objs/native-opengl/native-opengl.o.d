3b40a441b5827735514a1a2cbc3d9f2379d5fc8e
fjpalfer <fjpalfer@gmail.com>
2018-08-23 11:34:01 +0200

    Cleaning project.

--- AndroidOpenCVC/app/src/main/obj/local/arm64-v8a/objs/native-opengl/native-opengl.o.d
+++
@@ -1,7 +0,0 @@
-./obj/local/arm64-v8a/objs/native-opengl/native-opengl.o: \
-  jni/native-opengl.cpp jni/opengl_example.h \
-  jni/aopencvc_opengl_OpenGLJNIWrapper.h
-
-jni/opengl_example.h:
-
-jni/aopencvc_opengl_OpenGLJNIWrapper.h:
--- AndroidOpenCVC/app/src/main/obj/local/arm64-v8a/objs/native-opengl/opengl_example.o.d
+++
@@ -1,6 +0,0 @@
-./obj/local/arm64-v8a/objs/native-opengl/opengl_example.o: \
-  jni/opengl_example.c jni/opengl_example.h jni/../cpp/glwrapper.h
-
-jni/opengl_example.h:
-
-jni/../cpp/glwrapper.h:
